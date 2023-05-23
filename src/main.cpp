402043ad395a4f661b3a73d684d8750ec4aff34b
ruba <d.rubalskyi@pixelalabs.com>
2023-05-23 11:58:52 +0300

    Added sample and sockpp submodule

---
+++ src/main.cpp
@@ -0,0 +1,55 @@
+#include "sockpp/tcp_connector.h"
+
+#include <string>
+
+bool SendClusterCommand(sockpp::tcp_connector& Connector, std::string const& Cmd)
+{
+	uint32_t PacketSize = Cmd.length();
+	size_t HeaderSize = sizeof(PacketSize);
+
+	std::vector<uint8_t> Buffer(sizeof(uint32_t) + PacketSize);
+	
+	memcpy(Buffer.data(), &PacketSize, HeaderSize);
+	memcpy(Buffer.data() + HeaderSize, Cmd.data(), PacketSize);
+
+	ssize_t BytesWritten = Connector.write_n(Buffer.data(), Buffer.size());
+
+	if (BytesWritten != Buffer.size())
+	{
+		std::cerr << "Error writing to the TCP stream:"
+			<< "\n\t" << Connector.last_error_str() << std::endl;
+
+		return false;
+	}
+
+	return true;
+}
+
+int main()
+{
+	sockpp::initialize();
+	
+	std::string Host = "127.0.0.1";
+	in_port_t Port = 41003;
+
+	sockpp::tcp_connector Connector({ Host, Port }, std::chrono::seconds{ 5 });
+
+	if (!Connector) {
+		std::cerr << "Error connecting to server at "
+			<< sockpp::inet_address(Host, Port)
+			<< "\n\t" << Connector.last_error_str() << std::endl;
+		return 1;
+	}
+
+	std::cout << "Created a connection from " << Connector.address() << std::endl;
+
+	// Set a timeout for the responses
+	if (!Connector.read_timeout(std::chrono::seconds(5))) {
+		std::cerr << "Error setting timeout on TCP stream: "
+			<< Connector.last_error_str() << std::endl;
+	}
+
+	std::string Cmd = "{\"Name\": \"EventName\", \"Type\": \"EventType\", \"Category\": \"EventCategory\", \"Parameters\": {\"SomeField\":\"SomeField\"}}";
+
+	SendClusterCommand(Connector, Cmd);
+}
